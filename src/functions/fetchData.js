da1eff5a13adaefc9aaec23948ae9d55ff7a591e
ANireshkumar <139845367+ANireshkumar@users.noreply.github.com>
2025-01-20 08:05:31 +0530

    changin the api

--- src/functions/fetchData.js
+++ src/functions/fetchData.js
@@ -1,27 +1,49 @@
+// Function to fetch data from a given URL and execute a callback with the data
 const getData = (file, callback) => {
   fetch(file, {
     headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
+      "Content-Type": "application/json",
+      Accept: "application/json",
     },
   })
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
     .then((data) => {
       callback(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
     });
 };
 
+// Function to fetch data from the OMDB API
 export const Fetch = async () => {
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'ee341a7b55msh81bd6fcb70a206bp132128jsn077024b1aef2',
-      'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com',
-    },
-  };
-  const response = await fetch('https://imdb-top-100-movies.p.rapidapi.com/', options);
-  const data = await response.json();
-  return data;
+  const apiKey = "91e34a07"; 
+  const movieId = "tt3896198"; 
+  const url = `http://www.omdbapi.com/?i=${movieId}&apikey=${apiKey}`; 
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error; // Rethrow the error for further handling if needed
+  }
 };
 
 export default getData;

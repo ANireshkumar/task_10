62fba946323fedae6fafd2d70c8ea3942c227607
ANireshkumar <139845367+ANireshkumar@users.noreply.github.com>
2025-01-20 08:10:34 +0530

    Update axios.js

--- axios.js
+++ axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 /* to make requests to the movie database */
 const instance = axios.create({
-  baseURL: 'http://api.themoviedb.org/3',
+  baseURL: 'http://www.omdbapi.com',
 });
 
 export default instance;
